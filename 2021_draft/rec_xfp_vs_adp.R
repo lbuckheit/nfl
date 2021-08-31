cba6b8e926e94d5005b1c7bf905657363ffef317
Luke Buckheit <lukebuckheit@gmail.com>
2021-08-31 00:07:54 -0400

    2020 receivers

---
+++ 2021_draft/rec_xfp_vs_adp.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(ggrepel)
+library(ggimage)
+library(nflfastR)
+library(dplyr)
+library(ggplot2)
+options(scipen = 9999)
+
+xfp_rec_2020 <- read.csv(file = "./2021_draft/2020_xfp_rec.csv")
+
+adp_data <- read.csv(file = "./2021_draft/clean_adp_data.csv")
+
+merge <- merge(xfp_rec_2020, adp_data, by="gsis_id") %>%
+  # Filter in/out particular positions here
+  filter(grepl('WR', position))
+
+ggplot(merge, aes(x=adp, y=exp_hPPR_pts_pg, label=receiver)) +
+  geom_point() +
+  geom_text_repel() +
+  geom_smooth(method = "lm", se = FALSE)  
--- xfp_targets_mk2.R
+++ xfp_targets_mk2.R
@@ -37,13 +37,13 @@ pbp_df <- readRDS(url('https://raw.githubusercontent.com/guga31bb/nflfastR-data/
   # Grab smaller chunks of the season if you want
   filter(week >= 3 & week <= 17)
 
-avg_exp_fp_df <- pbp_df %>%
+avg_exp_fp_df <- pbp_df_2020 %>%
   # Get passing plays
   filter(pass_attempt==1 & season_type=='REG' & two_point_attempt==0 & !is.na(receiver_id)) %>%
   # Add our custom xyac columns
   add_xyac_dist %>%
   # Select relevant columns
-  select(season = season.x, game_id, play_id, posteam = posteam.x, receiver, yardline_100 = yardline_100.x, air_yards = air_yards.x, actual_yards_gained = yards_gained, complete_pass, cp, yac_prob = prob, gain) %>%
+  select(season = season.x, game_id, play_id, posteam = posteam.x, receiver, receiver_id, yardline_100 = yardline_100.x, air_yards = air_yards.x, actual_yards_gained = yards_gained, complete_pass, cp, yac_prob = prob, gain) %>%
   mutate(
     # Check whether the target is in the end zone (yardline_100==air_yards implies an end zone target)
     # The YAC function creates a copy of each play with a different gain number and then assigns a probability to the potential of that YAC happening
@@ -64,18 +64,18 @@ avg_exp_fp_df <- pbp_df %>%
     game_played = 0
   )  %>%
   # Indicating a single receiver game for use in the games played summary later
-  # TODO - One issue here is that if a receiver played but didn't receive a target, it won't count, when really it should count as a terrible game
+  # TODO - The games played count is a little fucked because it only registers games in which the player had a target I think
   group_by(game_id, receiver) %>%
   mutate(game_played = ifelse(row_number()==1,1,0)) %>%
   ungroup %>%
-  # TODO - NOT ACTUALLY SURE WHAT'S GOING ON HERE.  THINK IT HAS SOMETHING TO DO WITH ONLY RECORDING A SINGLE TARGET FOR THE MANY ROWS OF POTENTIAL YAC OUTCOMES
+  # TODO - NOT ACTUALLY SURE WHAT'S GOING ON HERE.  IT HAS SOMETHING TO DO WITH ONLY RECORDING A SINGLE TARGET FOR THE MANY ROWS OF POTENTIAL YAC OUTCOMES
   group_by(game_id, play_id, receiver) %>%
   mutate(target = ifelse(row_number()==1,1,0)) %>%
   ungroup %>%
-  group_by(posteam, receiver) %>%
+  group_by(posteam, receiver, receiver_id) %>%
   # Summary columns
   summarize(
-    # Games played by a receiver (na.rm removes N/A entries)
+    # Games played by a receiver (na.rm removed N/A entries)
     games = sum(game_played, na.rm = T),
     # Targets by a receiver
     targets = sum(target, na.rm = T),
@@ -93,18 +93,23 @@ avg_exp_fp_df <- pbp_df %>%
     exp_yards = sum(exp_yards, na.rm = T),
     # Expected touchdowns for a receiver
     exp_td = sum(ifelse(gain==yardline_100, catch_run_prob, 0), na.rm = T),
-    # Expected 0.t PPR points for a receiver
+    # Expected 0.5 PPR points for a receiver
     exp_hPPR_pts = sum(exp_hPPR_points, na.rm = T),
+    # Expected 0.5 PPR points per game
+    exp_hPPR_pts_pg = exp_hPPR_pts / games,
     # Difference between actual and expected 0.5 PPR points
     hPPR_over_exp = hPPR_pts - exp_hPPR_pts
   ) %>%
   ungroup
 
 short_xfp_targets <- avg_exp_fp_df %>%
-  select(receiver, games, hPPR_pts, exp_hPPR_pts, hPPR_over_exp) %>%
+  select(receiver, gsis_id = receiver_id, games, hPPR_pts, exp_hPPR_pts, exp_hPPR_pts_pg, hPPR_over_exp) %>%
   filter(hPPR_pts >= 10)
 
 ggplot(short_xfp_targets, aes(x=exp_hPPR_pts, y=hPPR_pts, label=receiver)) +
   geom_point() +
   geom_text_repel() +
   geom_smooth(method = "lm", se = FALSE)
+
+write.csv(short_xfp_targets, "./2021_draft/2020_xfp_rec.csv")
+
