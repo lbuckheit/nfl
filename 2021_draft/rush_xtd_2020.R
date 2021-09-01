dee57d63b2b5939c2ef649aee6326de56c221993
Luke Buckheit <lukebuckheit@gmail.com>
2021-08-31 22:02:34 -0400

    cleanup

--- 2021_draft/rush_xtd_2020.R
+++ 2021_draft/rush_xtd_2020.R
@@ -35,13 +35,36 @@ xtd_2020 <- pbp_df_2020 %>%
 
 xtd_2020_players <- xtd_2020 %>%
   group_by(rusher, rusher_id) %>%
-  summarize(carries = n(), games = length(unique(game_id)), xtd = sum(xtd)) %>%
-  filter(carries >= 20)
+  summarize(carries = n(), games = length(unique(game_id)), xtd = sum(xtd), xtd_pg = xtd / games) %>%
+  filter(xtd > 4)
 
 xtd_2020_teams <- xtd_2020 %>%
   group_by(posteam) %>%
-  summarize(carries = n(), games = length(unique(game_id)), xtd = sum(xtd))
+  summarize(carries = n(), xtd = sum(xtd))
 
+## Graphing
+
+# Total expected TDs by player
+ggplot(xtd_2020_players, aes(x = reorder(rusher, -xtd), y = xtd)) +
+  geom_bar(stat = "identity") +
+  theme(axis.text.x = element_text(angle = -90)) +
+  labs(x = "Player",
+       y = "Expected TDs",
+       title = "2020 Expected Rushing Touchdowns",
+       caption = "Via nflFastR"
+  )
+
+# Per-game expected TDs by player
+ggplot(xtd_2020_players, aes(x = reorder(rusher, -xtd_pg), y = xtd_pg)) +
+  geom_bar(stat = "identity") +
+  theme(axis.text.x = element_text(angle = -90)) +
+  labs(x = "Player",
+       y = "Expected TDs",
+       title = "2020 Expected Rushing Touchdowns",
+       caption = "Via nflFastR"
+  )
+
+# Total expected TDs by team
 ggplot(xtd_2020_teams, aes(x = reorder(posteam, -xtd), y = xtd)) +
   geom_bar(stat = "identity") +
   theme(axis.text.x = element_text(angle = -90)) +
